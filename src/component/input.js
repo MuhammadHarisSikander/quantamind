f054b43bf694af81722e4baeb4a12889d2bb028d
MuhammadHarisSikander <harissikander786@gmail.com>
2023-02-07 19:41:44 +0500

    Transport-pro

--- src/component/input.js
+++ src/component/input.js
@@ -2,19 +2,19 @@ import * as React from 'react';
 // import Box from '@mui/material/Box';
 // import TextField from '@mui/material/TextField';
 
-export default function BasicTextFields({ name, form }) {
+export default function BasicTextFields({ name, form, nameData, formData }) {
     return (
 
         <div style={{ display: 'flex', justifyContent: 'space-between', }}>
             <div style={{ display: 'flex' }}>
                 <p style={{ fontSize: '22px', paddingRight: '20px' }}>{name}</p>
 
-                <p style={{ fontSize: '22px' }}>Haris</p>
+                <p style={{ fontSize: '22px' }}>{nameData}</p>
             </div>
             <div style={{ display: 'flex' }}>
                 <p style={{ fontSize: '22px', paddingRight: '20px' }}>{form}</p>
 
-                <p style={{ fontSize: '22px' }}>12345</p>
+                <p style={{ fontSize: '22px' }}>{formData}</p>
             </div>
 
         </div>
--- src/screens/transportRequestForm.js
+++ src/screens/transportRequestForm.js
@@ -1,19 +1,167 @@
 import React from 'react';
 import Title from '../component/header'
 import CutomeInput from '../component/input';
+import Checkbox from '@mui/material/Checkbox';
+import Radio from '@mui/material/Radio';
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 function TransportRequestForm() {
+    const [selectedValue, setSelectedValue] = React.useState('a');
+    const handleChange = (event) => {
+        setSelectedValue(event.target.value);
+    };
+
     return (
         <div>
             <Title />
-            <div style={{ paddingLeft: '10%', paddingRight: '10%' }}>
-                <CutomeInput name={"Requestor Name: "} form={"Form No"} />
-                <hr />
-                <h3 style={{ textAlign: 'center' }}>Transport Request</h3>
-                <hr />
-                <CutomeInput name={"Number :  "} form={"Ref No."} />
-                <hr />
+            <div style={{ paddingLeft: '10%', paddingRight: '10%', }}>
+                <div style={{ border: '1px solid', padding: '20px', borderColor: '#C8C8C8' }}>
 
+                    <CutomeInput name={"Requestor Name: "} nameData={"Haris"} form={"Form No"} formData={"12345"} />
+                    <hr />
+                    <h3 style={{ textAlign: 'center' }}>Transport Request</h3>
+                    <hr />
+                    <CutomeInput name={"Number :  "} nameData={"12345"} form={"Ref No."} formData={"12345"} />
+                    <hr />
+                    <CutomeInput name={"Number : "} nameData={"Lorem apsum"} />
+                    <hr />
+
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <CutomeInput name={"Type : "} />
+                        <div style={{ flex: 1, justifyContent: 'space-evenly', display: 'flex' }}>
+                            <div style={{ display: 'flex' }}>
+                                <Checkbox {...label} defaultChecked /> <p style={{ display: 'flex', alignItems: 'center' }}>Customizing</p>
+                            </div>
+                            <div style={{ display: 'flex' }}>
+                                <Checkbox {...label} defaultChecked /> <p style={{ display: 'flex', alignItems: 'center' }}>Work Bench</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr />
+
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <CutomeInput name={"Released : "} />
+                        <div style={{ flex: 1, justifyContent: 'space-evenly', display: 'flex' }}>
+                            <div style={{ display: 'flex' }}>
+                                <Radio checked={selectedValue === 'a'} onChange={handleChange} value="a" name="radio-buttons" inputProps={{ 'aria-label': 'A' }} /> <p style={{ display: 'flex', alignItems: 'center' }}>Yes</p>
+                            </div>
+                            <div style={{ display: 'flex' }}>
+                                <Radio checked={selectedValue === 'a'} onChange={handleChange} value="a" name="radio-buttons" inputProps={{ 'aria-label': 'A' }} /> <p style={{ display: 'flex', alignItems: 'center' }}>No</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr />
+                    <CutomeInput name={"Special Instructions : "} nameData={"Lorem apsum"} />
+
+                    <hr />
+                    <h3 style={{ textAlign: 'center' }}>Priority</h3>
+                    <hr />
+
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <CutomeInput name={"Priority : "} />
+                        <div style={{ flex: 1, justifyContent: 'space-evenly', display: 'flex' }}>
+                            <div style={{ display: 'flex' }}>
+                                <Radio checked={selectedValue === 'a'} onChange={handleChange} value="a" name="radio-buttons" inputProps={{ 'aria-label': 'A' }} /> <p style={{ display: 'flex', alignItems: 'center' }}>Normal</p>
+                            </div>
+                            <div style={{ display: 'flex' }}>
+                                <Radio checked={selectedValue === 'a'} onChange={handleChange} value="a" name="radio-buttons" inputProps={{ 'aria-label': 'A' }} /> <p style={{ display: 'flex', alignItems: 'center' }}>Urgent</p>
+                            </div>
+                        </div>
+                    </div>
+                    <CutomeInput name={"Justifications: "} nameData={"Lorem apsum"} />
+                    <hr />
+
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <CutomeInput name={"Priority : "} />
+                        <div style={{ flex: 1, justifyContent: 'space-evenly', display: 'flex' }}>
+                            <div style={{ display: 'flex' }}>
+                                <Radio checked={selectedValue === 'a'} onChange={handleChange} value="a" name="radio-buttons" inputProps={{ 'aria-label': 'A' }} /> <p style={{ display: 'flex', alignItems: 'center' }}>Approve</p>
+                            </div>
+                            <div style={{ display: 'flex' }}>
+                                <Radio checked={selectedValue === 'a'} onChange={handleChange} value="a" name="radio-buttons" inputProps={{ 'aria-label': 'A' }} /> <p style={{ display: 'flex', alignItems: 'center' }}>Reject & Close</p>
+                            </div>
+                            <div style={{ display: 'flex' }}>
+                                <Radio checked={selectedValue === 'a'} onChange={handleChange} value="a" name="radio-buttons" inputProps={{ 'aria-label': 'A' }} /> <p style={{ display: 'flex', alignItems: 'center' }}>Return to</p>
+                            </div>
+                            <CutomeInput name={"LOV"} />
+                        </div>
+                    </div>
+                    <hr />
+
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <CutomeInput name={"Governance Manager:  "} />
+                        <div style={{ flex: 1, justifyContent: 'space-evenly', display: 'flex' }}>
+                            <div style={{ display: 'flex' }}>
+                                <Radio checked={selectedValue === 'a'} onChange={handleChange} value="a" name="radio-buttons" inputProps={{ 'aria-label': 'A' }} /> <p style={{ display: 'flex', alignItems: 'center' }}>Approve</p>
+                            </div>
+                            <div style={{ display: 'flex' }}>
+                                <Radio checked={selectedValue === 'a'} onChange={handleChange} value="a" name="radio-buttons" inputProps={{ 'aria-label': 'A' }} /> <p style={{ display: 'flex', alignItems: 'center' }}>Reject & Close</p>
+                            </div>
+                            <div style={{ display: 'flex' }}>
+                                <Radio checked={selectedValue === 'a'} onChange={handleChange} value="a" name="radio-buttons" inputProps={{ 'aria-label': 'A' }} /> <p style={{ display: 'flex', alignItems: 'center' }}>Return to</p>
+                            </div>
+                            <CutomeInput name={"LOV"} />
+                        </div>
+                    </div>
+
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <CutomeInput name={"GM ITS:  "} />
+                        <div style={{ flex: 1, justifyContent: 'space-evenly', display: 'flex' }}>
+                            <div style={{ display: 'flex' }}>
+                                <Radio checked={selectedValue === 'a'} onChange={handleChange} value="a" name="radio-buttons" inputProps={{ 'aria-label': 'A' }} /> <p style={{ display: 'flex', alignItems: 'center' }}>Approve</p>
+                            </div>
+                            <div style={{ display: 'flex' }}>
+                                <Radio checked={selectedValue === 'a'} onChange={handleChange} value="a" name="radio-buttons" inputProps={{ 'aria-label': 'A' }} /> <p style={{ display: 'flex', alignItems: 'center' }}>Reject & Close</p>
+                            </div>
+                            <div style={{ display: 'flex' }}>
+                                <Radio checked={selectedValue === 'a'} onChange={handleChange} value="a" name="radio-buttons" inputProps={{ 'aria-label': 'A' }} /> <p style={{ display: 'flex', alignItems: 'center' }}>Return to</p>
+                            </div>
+                            <CutomeInput name={"LOV"} />
+                        </div>
+                    </div>
+
+
+                    <hr />
+                    <h3 style={{ textAlign: 'center' }}>Basis Section</h3>
+                    <hr />
+                    <CutomeInput name={"Basis Admin: "} nameData={"Haris"} form={"Date: "} formData={"12345"} />
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <CutomeInput name={"Result: "} />
+                        <div style={{ flex: 1, justifyContent: 'space-evenly', display: 'flex' }}>
+                            <div style={{ display: 'flex' }}>
+                                <Radio checked={selectedValue === 'a'} onChange={handleChange} value="a" name="radio-buttons" inputProps={{ 'aria-label': 'A' }} /> <p style={{ display: 'flex', alignItems: 'center' }}>Success</p>
+                            </div>
+                            <div style={{ display: 'flex' }}>
+                                <Radio checked={selectedValue === 'a'} onChange={handleChange} value="a" name="radio-buttons" inputProps={{ 'aria-label': 'A' }} /> <p style={{ display: 'flex', alignItems: 'center' }}>Failure</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <CutomeInput name={"Justifications: "} nameData={"Lorem apsum"} />
+
+                    <div style={{ flex: 1, justifyContent: 'center', display: 'flex' }}>
+                        <div style={{ display: 'flex', marginRight: '5%' }}>
+                            <Checkbox {...label} defaultChecked /> <p style={{ display: 'flex', alignItems: 'center' }}>Yes</p>
+                        </div>
+                        <div style={{ border: '1px solid', paddingLeft: '5%', paddingRight: '5%' }}>
+                            <p>Upload Attachment</p>
+                        </div>
+                    </div>
+                    <hr />
+
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <CutomeInput name={"Acknowledge : "} />
+                        <div style={{ flex: 1, justifyContent: 'space-evenly', display: 'flex' }}>
+                            <div style={{ display: 'flex' }}>
+                                <Checkbox {...label} defaultChecked /> <p style={{ display: 'flex', alignItems: 'center' }}>Acknowledge</p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div >
     )
